6bfe7d21ad7d03f2444cb79fe69db9914af3c882
secminhr <secminhrian@gmail.com>
2021-04-30 01:16:37 +0800

    Map can draw the path you walked through.

--- app/src/main/java/ncku/pd2finalapp/MapActivity.java
+++ app/src/main/java/ncku/pd2finalapp/MapActivity.java
@@ -2,11 +2,15 @@ package ncku.pd2finalapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,14 +18,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
 
     private GoogleMap map;
+    private Polyline walkedPath;
     private boolean mapReady = false;
     private boolean viewReady = false;
     private void setMapReady(boolean value) {
@@ -83,7 +93,31 @@ public class MapActivity extends AppCompatActivity {
                 map.addMarker(new MarkerOptions().position(current).title("Current position"));
                 map.moveCamera(CameraUpdateFactory.newLatLng(current));
                 getSupportActionBar().setTitle("PD2FinalApp");
+                PolylineOptions points = new PolylineOptions().add(current);
+                walkedPath = map.addPolyline(points);
             });
+
+            LocationRequest request = LocationRequest.create()
+                                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                    .setInterval(5000);
+
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(@NonNull LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    Location last = locationResult.getLastLocation();
+                    LatLng newPoint = new LatLng(last.getLatitude(), last.getLongitude());
+                    List<LatLng> points = walkedPath.getPoints();
+                    points.add(newPoint);
+                    walkedPath.setPoints(points);
+                }
+
+                @Override
+                public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
+                    super.onLocationAvailability(locationAvailability);
+                    //do nothing
+                }
+            }, getMainLooper());
         } else {
             permissionRequester.launch(Manifest.permission.ACCESS_FINE_LOCATION);
         }
